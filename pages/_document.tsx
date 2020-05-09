b83ca09238202869f76ac65056161b6c31f83643
Frederik Bosch <frede.bosch@gmail.com>
2020-05-09 17:31:30 +0200

    added favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -44,6 +44,13 @@ export default class extends Document {
           <link rel="preconnect" href='https://res.cloudinary.com' crossOrigin='' />
           <title>Crittepedia</title>
           <meta name="description" content="An overview of all fish and bugs" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#74735f" />
+          <meta name="msapplication-TileColor" content="#f1f0dc" />
+          <meta name="theme-color" content="#EEEBC7"></meta>
         </Head>
         <body className={'no-js'}>
           <PageLoadScript />
