dc7208c01ddaeb17f88c2868a6f71f5cba39cbd6
Frederik Bosch <frede.bosch@gmail.com>
2020-05-10 02:58:28 +0200

    remove in-app splashscreen

--- components/GlobalStyle.tsx
+++ components/GlobalStyle.tsx
@@ -39,59 +39,6 @@ export default createGlobalStyle`
     background: linear-gradient(to right, rgb(234, 227, 169) 5%, #efe4b7 100%);
   }
 
-  .fade {
-    opacity: 0;
-    animation: fadeIn 250ms linear 100ms;
-    animation-fill-mode: forwards;
-  }
-
-  /* .js {
-    opacity: 0;
-    &.loaded {
-      animation: fadeIn 800ms cubic-bezier(0,.5,.5,1);
-      animation-fill-mode: forwards;
-    }
-  } */
-
-  .js {
-    @media ${device.laptop} {
-      main:before {
-        display: none;
-      }
-    }
-    &.loaded {
-      main:before {
-        opacity: 0;
-        background-size: 12vh;
-        z-index: -1;
-      }
-    }
-    main:before {
-      content: '';
-      position: fixed;
-      height: calc(100% + env(safe-area-inset-top));
-      width: 100%;
-      background: ${props => props.theme.orangeAccent};
-      z-index: 99;
-      top: calc(0% - env(safe-area-inset-top));
-      opacity: 1;
-      transition: opacity 800ms cubic-bezier(0,.5,.5,1) 200ms, z-index 0ms linear 1s, background-size 600ms ease;
-      background-image: url(${logo});
-      background-size: 10vh;
-      background-repeat: no-repeat;
-      background-position: center;
-    }
-  }
-
-  @keyframes splashScreenFadeOut {
-    0% {
-      opacity: 1;
-    }
-    100% {
-      opacity: 0;
-    }
-  }
-
   @keyframes fadeIn {
     0% {
       opacity: 0;
