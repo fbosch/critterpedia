c627d9cb9d14935d81ef5ffd769345b12a5300dd
Frederik Bosch <frede.bosch@gmail.com>
2020-05-10 02:21:01 +0200

    added IOS splash screens

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -52,7 +52,15 @@ export default class extends Document {
           <meta name="apple-mobile-web-app-status-bar-style" content="default" />
           <meta name="msapplication-TileColor" content="#F8CC63" />
           <meta name="theme-color" content="#F8CC63" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+          <link rel="apple-touch-startup-image" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)" href="/apple-launch-1125x2436.png" />
+          <link rel="apple-touch-startup-image" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)" href="/apple-launch-750x1334.png" />
+          <link rel="apple-touch-startup-image" media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3)" href="/apple-launch-1242x2208.png" />
+          <link rel="apple-touch-startup-image" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)" href="/apple-launch-640x1136.png" />
+          <link rel="apple-touch-startup-image" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)" href="/apple-launch-1536x2048.png" />
+          <link rel="apple-touch-startup-image" media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)" href="/apple-launch-1668x2224.png" />
+          <link rel="apple-touch-startup-image" media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)" href="/apple-launch-2048x2732.png" />
         </Head>
         <body>
           <PageLoadScript />
