a6360e9687a333e87ccac0b4583f9d5431c23ef5
Frederik Bosch <frede.bosch@gmail.com>
2020-05-10 03:05:41 +0200

    change ios top bar

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -49,7 +49,7 @@ export default class extends Document {
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
           <link rel="manifest" href="/site.webmanifest" />
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#74735f" />
-          <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
           <meta name="msapplication-TileColor" content="#F8CC63" />
           <meta name="theme-color" content="#F8CC63" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
