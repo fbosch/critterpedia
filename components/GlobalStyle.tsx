1cf05c2286a834f84a5b63afd95807d1b82c3f63
Frederik Bosch <frede.bosch@gmail.com>
2020-05-15 21:20:32 +0200

    fix: font display

--- components/GlobalStyle.tsx
+++ components/GlobalStyle.tsx
@@ -63,7 +63,7 @@ export default createGlobalStyle`
 
   @font-face {
     font-family: Humming;
-    font-display: fallback;
+    font-display: swap;
     src: url("./assets/fonts/humming.otf") format("opentype");
   }
 
